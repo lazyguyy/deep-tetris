cc47ea3b4da42958b0d5f86deb90848fad4647f8
lazyguyy <17795787+lazyguyy@users.noreply.github.com>
2019-02-19 17:42:12 +0100

    keyboard tetris now works perfectly

--- keyboard_player.py
+++ keyboard_player.py
@@ -1,3 +1,6 @@
+import logic.tetris as tetris
+import numpy as np
+
 
 def character(num):
     return ' ' if not num else '#'
@@ -6,33 +9,35 @@ def character(num):
 def render_board(board):
     for l in range(len(board)):
         line = ''.join(character(e) for e in board[l, :])
-        print(line)
+        print(f"|{line}|")
 
 
 def get_move():
     while True:
-        move = input("ENTER MOVE (R/L/U/N)").lower()
-        if move in 'rlnu':
+        move = input("ENTER MOVE (W/A/S/D)").lower()
+        if move in 'wasd' or not move:
             return move
         print("Illegal move!")
 
 
 def main():
 
-    board = ...
+    board = tetris.tetris_batch(1, drop_every=2)
 
-    move = get_move()
-    if move == 'r':
-        ...
-    elif move == 'l':
-        ...
-    elif move == 'u':
-        ...
-    elif move == 'n':
-        ...
+    while True:
+        render_board(board.get_boards()[0])
+        move = get_move()
+        if move == 'd':
+            move = np.array([1])
+        elif move == 'a':
+            move = np.array([0])
+        elif move == 'w':
+            move = np.array([2])
+        else:
+            move = np.array([3])
 
-    pass
-
+        points, lost = board.make_moves(move)
+        print(points[0], ["", "GAME OVER"][lost[0]])
 
 if __name__ == '__main__':
     main()
