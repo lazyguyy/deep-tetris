620bf61cbd9cbb841b236f90a642b66d7d081349
slyphiX <slyphiX@users.noreply.github.com>
2019-02-19 13:16:57 +0100

    changed formatting

--- tetris-game/tetris.py
+++ tetris-game/tetris.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-tiles = [
+BASIC_TILES = [
     # ##
     # ##
     [[1, 1],
@@ -41,7 +41,7 @@ def generate_all_rotations(tile):
     matrix = np.asmatrix(tile, dtype=np.int32)
     return matrix, np.rot90(matrix), np.rot90(matrix, 2), np.rot90(matrix, -1)
 
-tiles = np.array([generate_all_rotations(tile) for tile in tiles])
+TILES = np.array([generate_all_rotations(tile) for tile in BASIC_TILES])
 
 def test_single_tile(board, tile, position):
     x, y = position[0] + 1, position[1] + 1
@@ -58,7 +58,15 @@ def clear_single_board(board):
 vectorized_clear = np.vectorize(clear_single_board, signature="(m,n)->()")
 
 def spawn_new_tile():
-    return np.random.choice(tiles), [0, 0]
+    return np.random.choice(TILES), [0, 0]
+
+
+MOVE_LEFT = 0
+MOVE_RIGHT = 1
+ROTATE_CW = 2
+ROTATE_CCW = 3
+DO_NOTHING = 4
+
 
 class tetris_batch():
 
@@ -75,7 +83,7 @@ class tetris_batch():
         # views so the walls are hidden
         self.views = np.array([board[:,1:-1] for board in self.boards])
         # current tile for each board
-        self.tiles = np.random.choice(len(tiles), batch_size, True)
+        self.tiles = np.random.choice(len(TILES), batch_size, True)
         # current position of each tile for each board
         self.positions = np.array([[0,0] for _ in range(batch_size)])
         # current rotation of each tile for each board
@@ -92,15 +100,11 @@ class tetris_batch():
     # 3 -> rotate counterclockwise
     # 4 -> do nothing
     def make_moves(self, moves):
-        self.positions[moves == 0] += [0,-1]
-        self.positions[moves == 1] += [0, 1]
-        self.rotations[moves == 2] += -1
-        self.rotations[moves == 3] +=  1
+        self.positions[moves == MOVE_LEFT] += [0, -1]
+        self.positions[moves == MOVE_RIGHT] += [0, 1]
+        self.rotations[moves == ROTATE_CW] += -1
+        self.rotations[moves == ROTATE_CCW] += 1
         self.current_move += 1
 
-        is_not_okay = test_multiple_tiles(self.boards, tiles[self.tiles, self.rotations % 4], self.positions)
+        is_not_okay = test_multiple_tiles(self.boards, TILES[self.tiles, self.rotations % 4], self.positions)
 
-
-batch = tetris_batch(10)
-
-board = np.array([[1, 0, 0], [1, 1, 1], [0, 0, 1], [1, 1, 1]])
