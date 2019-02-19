b18b0d0ea0fd8841e9c2c844c7a99ea01e84b4df
slyphiX <slyphiX@users.noreply.github.com>
2019-02-19 20:34:32 +0100

    widget demo

--- ui-test.py
+++ ui-test.py
@@ -2,26 +2,40 @@ import kivy
 kivy.require('1.10.1')
 
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.graphics import *
+from kivy.uix.widget import Widget
+
+class FixedRatioLayout(AnchorLayout):
+    def __init__(self, ratio=2.0):
+        super().__init__(anchor_x='center', anchor_y='center')
+        self.ratio = ratio
+
+    def do_layout(self, *largs):
+        w, h = self.size
+        if h > w * self.ratio:
+            w = h / self.ratio
+        else:
+            h = w * self.ratio
+
+        for child in self.children:
+            child.size_hint = None, None
+            child.size = w, h
+        super().do_layout(*largs)
 
 
-class LoginScreen(GridLayout):
-
+class TetrisGame(Widget):
     def __init__(self, **kwargs):
-        super(LoginScreen, self).__init__(**kwargs)
-        self.cols = 2
-        self.add_widget(Label(text='User Name'))
-        self.username = TextInput(multiline=False)
-        self.add_widget(self.username)
-        self.add_widget(Label(text='password'))
-        self.password = TextInput(password=True, multiline=False)
-        self.add_widget(self.password)
+        super().__init__(**kwargs)
+        with self.canvas:
+            Rectangle(size=(50, 50))
+
 
 class MyApp(App):
     def build(self):
-        return LoginScreen()
+        layout = FixedRatioLayout()
+        layout.add_widget(TetrisGame())
+        return layout
 
 
 if __name__ == '__main__':
