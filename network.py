7c8d3d7d69ec6b4e54917490542e1bb73b7ae1fa
lazyguyy <17795787+lazyguyy@users.noreply.github.com>
2019-02-22 13:42:10 +0100

    fixing stuff with david (1)

--- network.py
+++ network.py
@@ -7,22 +7,28 @@ from keyboard_player import render_board
 
 
 NUM_TILES = len(tetris.TILES)
-NUM_EPISODES = 2**5
-BATCH_SIZE = 2**7
-LOSSES_PER_EPISODE = 2 * BATCH_SIZE
-PENALTY_PER_LOSS = 10
+NUM_EPISODES = 2**9
+BATCH_SIZE = 2**8
+LOSSES_PER_EPISODE = 2**4 * BATCH_SIZE
+PENALTY_PER_LOSS = 30
 WIDTH, HEIGHT = 10, 20
 EMA_FACTOR = 0.999
-RANDOM_MOVE_PROBABILITY = 0.1
+RANDOM_MOVE_PROBABILITY = 0.995
+DECAY = 0.999
 
 dtype = tf.float64
 
 def make_network(depths, tile_id, activation=None, bias=True):
     one_hot_tile_id = tf.one_hot(tile_id, NUM_TILES, dtype=dtype)
 
+    depths = tf.layers.batch_normalization(depths, training=True)
     input_layer = tf.concat([one_hot_tile_id, depths], axis=-1)
-    output_rotation = tf.layers.dense(input_layer, 4, activation=activation, use_bias=bias)
-    output_column = tf.layers.dense(input_layer, WIDTH, activation=activation, use_bias=bias)
+    input_layer = tf.layers.dense(input_layer, 64, activation=tf.nn.relu, use_bias=bias)
+
+    output_rotation = tf.layers.dense(input_layer, 4, activation=activation, use_bias=False)
+    output_column = tf.layers.dense(input_layer, WIDTH, activation=activation, use_bias=False)
+
+    output_column = tf.nn.softmax(output_column)
 
     return output_rotation, output_column
 
@@ -40,37 +46,45 @@ class depths_network:
         self.modified_rotation = tf.placeholder(shape=(None, 4), dtype=dtype, name="modified_rotations")
         self.modified_column = tf.placeholder(shape=(None, WIDTH), dtype=dtype, name="modified_columns")
 
-        self.output_rotation, self.output_column = make_network(self.depths, self.tile_ids, activation=tf.nn.leaky_relu)
+        self.output_rotation, self.output_column = make_network(self.depths, self.tile_ids, activation=None)
 
         self.loss = make_loss(self.output_rotation, self.output_column, self.modified_rotation, self.modified_column)
-        self.optimizer = tf.train.GradientDescentOptimizer(0.5).minimize(self.loss)
+        self.optimizer = tf.train.GradientDescentOptimizer(1e-3).minimize(self.loss)
 
     def train(self, num_episodes=NUM_EPISODES):
         with tf.Session() as sess:
             sess.run(tf.global_variables_initializer())
 
-            episodes = tqdm.tqdm(range(num_episodes))
-            for episode in episodes:
-                game = tetris.tetris_batch(BATCH_SIZE)
+            placed_tiles = 0
+            for episode in range(num_episodes):
+                game = tetris.tetris_batch(BATCH_SIZE, rows=10)
 
                 lost_games = 0
+                # with tqdm.tqdm(total=LOSSES_PER_EPISODE) as pbar:
                 while lost_games < LOSSES_PER_EPISODE:
-                    render_board(game.get_boards()[:4])
+                    placed_tiles += 1
+                    render_board(game.get_boards()[:1])
+                    print(game.score[:1])
                     old_depths = np.copy(game.depths)
                     old_tile_ids = np.copy(game.tiles)
                     rotation_quality, column_quality = sess.run((self.output_rotation, self.output_column), feed_dict={
                         self.depths: old_depths,
                         self.tile_ids: old_tile_ids
                         })
+                    print("Random bound, iterations: ", max(0.1, RANDOM_MOVE_PROBABILITY  * DECAY ** placed_tiles), placed_tiles)
+                    print("Depths:", old_depths[0])
+                    print("Tile Ids:", old_tile_ids[0])
+                    print("Output:", np.round(column_quality[0], 2))
 
                     rot = np.argmax(rotation_quality, axis=-1)
                     col = np.argmax(column_quality, axis=-1)
 
-                    if np.random.uniform(0, 1) < RANDOM_MOVE_PROBABILITY:
-                        rot = np.random.choice(4, BATCH_SIZE, True)
-                        col = np.random.choice(WIDTH, BATCH_SIZE, True)
+                    # if np.random.uniform(0, 1) < max(0.1, RANDOM_MOVE_PROBABILITY  * DECAY ** placed_tiles):
+                    #     rot = np.random.choice(4, BATCH_SIZE, True)
+                    #     col = np.random.choice(WIDTH, BATCH_SIZE, True)
 
                     reward, lost = game.drop_in(col, rot)
+                    # print(reward)
 
                     next_rotation_quality, next_column_quality = sess.run((self.output_rotation, self.output_column), feed_dict={
                         self.depths: game.depths,
